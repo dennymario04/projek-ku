dd386f55a83a2f03339600dd1b5e976c90afe24e
dennymario04 <denny.mario.business@gmail.com>
2022-03-12 12:51:48 +0700

    mengubah script insert ke OOP

--- component/insertDb.php
+++ component/insertDb.php
@@ -12,8 +12,7 @@ if(isset($_POST['submit'])){
     //query sql
     $sql = "INSERT INTO calon_siswa(nama,alamat,noHp,email,jurusan,asal_sekolah) 
             VALUES('$nama','$alamat','$noHp','$email','$jurusan','$asalSekolah')";
-    $query = mysqli_query($conn,$sql);
-    if($query){
+    if($conn -> query($sql)===TRUE){
         echo "<script>alert('Data sudah berhasil tersimpan');window.location='../index.php'</script>";
     }else{
         echo "<script>alert('Data Gagal disimpan,cek kembali!');window.location='../component/form_daftar.php'</script>";
