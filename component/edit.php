9e1104440f6517ccfeec8544eeda7dc2174000ba
dennymario04 <denny.mario.business@gmail.com>
2022-05-26 21:35:08 +0700

    edit script update anti sql injection

--- component/edit.php
+++ component/edit.php
@@ -11,11 +11,14 @@ if(isset($_POST['submit'])){
     $asalSekolah = $_POST['asal_sekolah'];
 
     //query sql
-    $sql = "UPDATE calon_siswa SET nama ='$nama',alamat = '$alamat', noHp = '$noHp', email = '$email', jurusan = '$jurusan', asal_sekolah = '$asalSekolah'
-            WHERE id_pendaftar = $id";
-    if($conn -> query($sql)===TRUE){
-        echo "<script>alert('Data sudah diupdate');window.location='../component/view.php'</script>";
-    }else{
+   // $sql = "UPDATE calon_siswa SET nama ='$nama',alamat = '$alamat', noHp = '$noHp', email = '$email', jurusan = '$jurusan', asal_sekolah = '$asalSekolah'
+     //       WHERE id_pendaftar = $id";
+    $stmt = $conn -> prepare("UPDATE calon_siswa SET nama =?,alamat = ?,noHp = ?, email = ?, jurusan = ?, asal_sekolah = ? WHERE id_pendaftar = ?");
+    $stmt -> bind_param('ssssssi',$_POST['nama'],$_POST['alamat'],$_POST['noHp'],$_POST['email'],$_POST['jurusan'],$_POST['asal_sekolah'],$_POST['id_pendaftar']);
+    $stmt -> execute();
+    if($stmt->affected_rows === 0){
         echo "<script>alert('Data gagal diupdate');window.location='../component/form_edit.php'</script>";
+    }else{
+        echo "<script>alert('Data sudah diupdate');window.location='../component/view.php'</script>";
     }
 }
