f5e91b06931f7a812d735cc588f30de62da23fde
dennymario04 <denny.mario.business@gmail.com>
2022-05-26 17:56:30 +0700

    Edit Script delete untuk anti sql injection

--- component/hapus.php
+++ component/hapus.php
@@ -5,11 +5,14 @@ if(isset($_GET['id_pendaftar'])){
     //ambil id dari tabel
     $id = $_GET['id_pendaftar'];
     //buat query untuk menghapus data di tabel
-    $sql = "DELETE FROM calon_siswa WHERE id_pendaftar=$id";
-
-    if($conn -> query($sql)===TRUE){
-        echo "<script>alert('Data sudah dihapus');window.location='../component/view.php'</script>";
-    }else{
+    //$sql = "DELETE FROM calon_siswa WHERE id_pendaftar=$id";
+    $stmt = $conn -> prepare("DELETE FROM calon_siswa WHERE id_pendaftar=?");
+    $stmt -> bind_param('i',$_GET['id_pendaftar']);
+    $stmt -> execute();
+    if($stmt->affected_rows === 0){
         echo "<script>alert('Data tidak bisa dihapus');window.location='../component/view.php'</script>";
+    }else{
+        echo "<script>alert('Data berhasil dihapus');window.location='../component/view.php'</script>";
     }
+
 }
