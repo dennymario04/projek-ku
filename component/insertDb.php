2e7087ff192c58c282e8e57844da08981e270251
dennymario04 <denny.mario.business@gmail.com>
2022-05-31 11:37:48 +0700

    Edit insertdb.php

--- component/insertDb.php
+++ component/insertDb.php
@@ -1,24 +1,3 @@
 <?php
 require "../Config/conf.php";
-if(isset($_POST['submit'])){
-    //ambil data dari form
-    $nama = $_POST['nama']; 
-    $alamat = $_POST['alamat'];
-    $noHp = $_POST['noHp'];
-    $email = $_POST['email'];
-    $jurusan = $_POST['jurusan'];
-    $asalSekolah = $_POST['asal_sekolah'];
-
-    //query sql
-    //$sql = "INSERT INTO calon_siswa(nama,alamat,noHp,email,jurusan,asal_sekolah) 
-            //VALUES('$nama','$alamat','$noHp','$email','$jurusan','$asalSekolah')";
-
-    $stmt = $conn -> prepare("INSERT INTO calon_siswa(nama,alamat,noHp,email,jurusan,asal_sekolah) VALUES (?,?,?,?,?,?)");
-    $stmt->bind_param('ssssss',$_POST['nama'],$_POST['alamat'],$_POST['noHp'],$_POST['email'],$_POST['jurusan'],$_POST['asal_sekolah']);
-    $stmt->execute();
-    if($stmt->affected_rows === 0){
-        echo "<script>alert('Data Gagal disimpan,cek kembali!');window.location='../component/form_daftar.php'</script>";
-    }else{
-        echo "<script>alert('Data sudah berhasil tersimpan');window.location='../index.php'</script>";
-    }
-}
+$Fungsi -> insertData();
