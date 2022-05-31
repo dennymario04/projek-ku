b73a00743c3f284d5ec0b65686caf34cf3e1ec24
dennymario04 <denny.mario.business@gmail.com>
2022-05-31 10:16:32 +0700

    Menambahkan testunit

--- Config/conf.php
+++ Config/conf.php
@@ -1,4 +1,24 @@
 <?php
+
+DEFINE('DB_HOST','localhost');
+DEFINE('DB_USER','root');
+DEFINE('DB_PASS','');
+DEFINE('DB_NAME','sekolahit');
+
+class Database{
+    public $conn;
+
+    function __construct(){
+        $this -> conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        if(msyqli_connect_error()){
+            die("Tidak berhasil menghubungkan ke Database!!".mysqli_connect_error());
+        }else{
+            echo "console.log('Database Connected Successfully')";
+        }
+    }
+}
+
+/*
 $serverName = "localhost";
 $user = "root";
 $password = "";
@@ -9,3 +29,4 @@ $conn = new mysqli($serverName,$user,$password,$database);
 if(mysqli_connect_error()){
     die("Tidak berhasil menghubungkan ke Database!!".mysqli_connect_error());
 }
+*/
