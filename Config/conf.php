4b53131dc6dfbc4dd1964818fb5c14afa3d0d804
dennymario04 <denny.mario.business@gmail.com>
2022-05-31 12:27:37 +0700

    EDIT script hapus menjadi oop

--- Config/conf.php
+++ Config/conf.php
@@ -103,6 +103,23 @@ class Fungsi extends Database{
             }
         }
     }
+    function hapusData(){
+        if(isset($_GET['id_pendaftar'])){
+            //ambil id dari tabel
+            $id = $_GET['id_pendaftar'];
+            //buat query untuk menghapus data di tabel
+            //$sql = "DELETE FROM calon_siswa WHERE id_pendaftar=$id";
+            $stmt = $this -> conn -> prepare("DELETE FROM calon_siswa WHERE id_pendaftar=?");
+            $stmt -> bind_param('i',$_GET['id_pendaftar']);
+            $stmt -> execute();
+            if($stmt->affected_rows === 0){
+                echo "<script>alert('Data tidak bisa dihapus');window.location='../component/view.php'</script>";
+            }else{
+                echo "<script>alert('Data berhasil dihapus');window.location='../component/view.php'</script>";
+            }
+        
+        }
+    }
 }
 
 
--- component/hapus.php
+++ component/hapus.php
@@ -1,18 +1,3 @@
 <?php
 require '../Config/conf.php';
-
-if(isset($_GET['id_pendaftar'])){
-    //ambil id dari tabel
-    $id = $_GET['id_pendaftar'];
-    //buat query untuk menghapus data di tabel
-    //$sql = "DELETE FROM calon_siswa WHERE id_pendaftar=$id";
-    $stmt = $conn -> prepare("DELETE FROM calon_siswa WHERE id_pendaftar=?");
-    $stmt -> bind_param('i',$_GET['id_pendaftar']);
-    $stmt -> execute();
-    if($stmt->affected_rows === 0){
-        echo "<script>alert('Data tidak bisa dihapus');window.location='../component/view.php'</script>";
-    }else{
-        echo "<script>alert('Data berhasil dihapus');window.location='../component/view.php'</script>";
-    }
-
-}
+$Fungsi -> hapusData();
