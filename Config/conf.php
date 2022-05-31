7a44d9c8ebf1163c674c63b01a5c45c81ce53ca6
dennymario04 <denny.mario.business@gmail.com>
2022-05-31 10:22:24 +0700

    Mengubah script menjadi OOP

--- Config/conf.php
+++ Config/conf.php
@@ -6,17 +6,20 @@ DEFINE('DB_PASS','');
 DEFINE('DB_NAME','sekolahit');
 
 class Database{
-    public $conn;
+    public $conn = "";
 
     function __construct(){
         $this -> conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-        if(msyqli_connect_error()){
+        if(mysqli_connect_errno()){
             die("Tidak berhasil menghubungkan ke Database!!".mysqli_connect_error());
         }else{
-            echo "console.log('Database Connected Successfully')";
+            echo "<script>console.log('Database Connected');</script>";
         }
     }
 }
+
+$DBConn = new Database();
+
 
 /*
 $serverName = "localhost";
