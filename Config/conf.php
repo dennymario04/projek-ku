050b2c4d4dcf1b241e89284997328008d4fce00c
dennymario04 <denny.mario.business@gmail.com>
2022-05-31 11:33:36 +0700

    Menambahkan fungsi insert

--- Config/conf.php
+++ Config/conf.php
@@ -7,7 +7,7 @@ DEFINE('DB_NAME','sekolahit');
 
 class Database{
     public $conn = "";
-
+    #fungsi koneksi ke database
     function __construct(){
         $this -> conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
         if(mysqli_connect_errno()){
@@ -19,14 +19,35 @@ class Database{
 }
 
 class Fungsi extends Database{
+    #fungsi select
     function viewData(){
         $result = mysqli_query($this -> conn ,"SELECT * FROM calon_siswa");
-       if($result ->num_rows > 0){
-        echo 
-        "<script>
-            console.log('Data Fetch');
-        </script>";
-       }
+        while($data = mysqli_fetch_array($result)){
+            $row[] = $data;
+      }
+      return $row;
+    }
+
+    #fungsi insert
+    function insertData(){
+        if(isset($_POST['submit'])){
+            //ambil data dari form
+            $nama = $_POST['nama']; 
+            $alamat = $_POST['alamat'];
+            $noHp = $_POST['noHp'];
+            $email = $_POST['email'];
+            $jurusan = $_POST['jurusan'];
+            $asalSekolah = $_POST['asal_sekolah'];
+            //Operasi insert
+            $stmt = $conn -> prepare("INSERT INTO calon_siswa(nama,alamat,noHp,email,jurusan,asal_sekolah) VALUES (?,?,?,?,?,?)");
+            $stmt->bind_param('ssssss',$_POST['nama'],$_POST['alamat'],$_POST['noHp'],$_POST['email'],$_POST['jurusan'],$_POST['asal_sekolah']);
+            $stmt->execute();
+            if($stmt->affected_rows === 0){
+                echo "<script>alert('Data Gagal disimpan,cek kembali!');window.location='../component/form_daftar.php'</script>";
+            }else{
+                echo "<script>alert('Data sudah berhasil tersimpan');window.location='../index.php'</script>";
+            }
+        }
     }
 }
 
@@ -34,9 +55,11 @@ class Fungsi extends Database{
 
 
 
+
 $DBConn = new Database();
-$View = new Fungsi();
-$View -> viewData();
+$Fungsi = new Fungsi();
+
+
 /*
 $serverName = "localhost";
 $user = "root";
